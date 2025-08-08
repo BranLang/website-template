172edf12c2351ffd2c42adec439dec619c9d31c1
Bran Lang <64263680+BranLang@users.noreply.github.com>
2025-08-08 15:45:34 +0200

    add karma config

---
+++ frontend/karma.conf.js
@@ -0,0 +1,47 @@
+const { join } = require('path');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage')
+    ],
+    client: {
+      jasmine: {}
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ],
+      check: {
+        global: {
+          statements: 50,
+          branches: 50,
+          functions: 50,
+          lines: 50
+        }
+      },
+      skipFilesWithNoCoverage: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    restartOnFileChange: true
+  });
+};
--- frontend/src/app/components/header/header.component.spec.ts
+++ frontend/src/app/components/header/header.component.spec.ts
@@ -2,6 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { LanguageService } from '../../services/language.service';
 import { SiteService } from '../../services/site.service';
+import { AuthService } from '../../services/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { of } from 'rxjs';
 
 class MockLanguageService {
@@ -14,21 +18,36 @@ class MockSiteService {
   resolveMediaUrl() { return ''; }
 }
 
+class MockAuthService {
+  isAdmin() { return false; }
+}
+
+@Pipe({ name: 'translate', standalone: true })
+class MockTranslatePipe implements PipeTransform {
+  transform(value: any): any { return value; }
+}
+
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let languageService: MockLanguageService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent],
+      imports: [RouterTestingModule, HeaderComponent],
       providers: [
         { provide: LanguageService, useClass: MockLanguageService },
-        { provide: SiteService, useClass: MockSiteService }
+        { provide: SiteService, useClass: MockSiteService },
+        { provide: AuthService, useClass: MockAuthService }
       ]
+    }).overrideComponent(HeaderComponent, {
+      remove: { imports: [TranslateModule] },
+      add: { imports: [MockTranslatePipe] }
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    languageService = TestBed.inject(LanguageService) as any;
     fixture.detectChanges();
   });
 
@@ -38,5 +57,18 @@ describe('HeaderComponent', () => {
 
   it('should default to Slovak language', () => {
     expect(component.currentLanguage).toBe('sk');
+  });
+
+  it('should change language', () => {
+    component.onLanguageChange('en');
+    expect(languageService.setLanguage).toHaveBeenCalledWith('en');
+    expect(component.currentLanguage).toBe('en');
+  });
+
+  it('should toggle mobile menu', () => {
+    component.toggleMobileMenu();
+    expect(component.mobileMenuOpen).toBeTrue();
+    component.closeMobileMenu();
+    expect(component.mobileMenuOpen).toBeFalse();
   });
 });
